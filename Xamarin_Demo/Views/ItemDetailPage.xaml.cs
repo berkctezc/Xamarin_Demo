f728ea7e3bac1dc67dd03527246005eef6e5ae8d
Berkcan Tezcaner <berkcantezcaner@gmail.com>
2021-05-20 19:41:56 +0300

    Add project files.

---
+++ Xamarin_Demo/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin_Demo.ViewModels;
+
+namespace Xamarin_Demo.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
---
+++ Xamarin_Demo/Views/NewItemPage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Xamarin_Demo.Models;
+using Xamarin_Demo.ViewModels;
+
+namespace Xamarin_Demo.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
